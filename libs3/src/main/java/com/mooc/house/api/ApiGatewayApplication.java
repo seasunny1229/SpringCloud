bb372a94c7bee68aaa34e81907286ee3bea52d11
cuiting <sea_sunny1229@163.com>
2020-09-02 23:31:13 +0800

    ADD: import libs-test

--- libs3/src/main/java/com/mooc/house/api/ApiGatewayApplication.java
+++ libs3/src/main/java/com/mooc/house/api/ApiGatewayApplication.java
@@ -1,5 +1,7 @@
 package com.mooc.house.api;
 
+import com.seasunny.libs_test.MyClass;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -17,6 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ApiGatewayApplication {
 
     public static void main(String[] args){
+        MyClass.test();
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 

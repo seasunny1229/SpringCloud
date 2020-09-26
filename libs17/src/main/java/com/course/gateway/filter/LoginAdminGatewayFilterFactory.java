445f0b395b330daec68f7e17c0486f07e9baa428
cuiting <sea_sunny1229@163.com>
2020-09-26 17:38:20 +0800

    add gateway

---
+++ libs17/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilterFactory.java
@@ -0,0 +1,23 @@
+package com.course.gateway.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class LoginAdminGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    @Resource
+    LoginAdminGatewayFilter loginAdminGatewayFilter;
+
+    @Autowired
+    private TestGatewayFilter testGatewayFilter;
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return testGatewayFilter;
+    }
+}

e5d91af013661783ae452bc0ba7472de8b63cc81
cuiting <sea_sunny1229@163.com>
2020-10-10 23:02:50 +0800

    web file

--- libs14/src/main/java/com/course/file/FileApplication.java
+++ libs14/src/main/java/com/course/file/FileApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 @MapperScan("com.course.file.mapper")
 public class FileApplication {
     public static void main(String[] args){
